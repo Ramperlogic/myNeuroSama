a866cd6f8d50a9e31fb33be0368127f4e1fa812a
Ramperlogic <1509319438@qq.com>
2025-02-18 23:56:56 +0800

    在ChatApplication初始化时初始化ollama Client

--- ChatApplication.py
+++ ChatApplication.py
@@ -1,11 +1,14 @@
 import ollama
-import requests
+
 
 class ChatApplication:
     def __init__(self, debug_mod, model):
         self.DEBUG_MOD = debug_mod
         self.MODEL = model
-        pass
+        self.ollama = ollama.Client(
+            host='http://host.docker.internal:11434',
+            headers={'Content-Type': 'application/json'}
+        )
 
     def run(self):
         print("-------------------- started --------------------")
@@ -14,11 +17,7 @@ class ChatApplication:
             if user_input.lower() == "exit":
                 print("-------------------- ending --------------------")
                 break
-            client = ollama.Client(
-                host='http://host.docker.internal:11434',
-                headers={'Content-Type': 'application/json'}
-            )
-            resp = client.chat(
+            resp = self.ollama.chat(
                 model=self.MODEL,
                 messages=[
                     {'role': 'user', 'context': user_input}
@@ -37,24 +36,3 @@ class ChatApplication:
                     print(f"Ai: {resp}")
             else:
                 print("error no resp")
-    def call_ollama(self):
-        user_input = input("You: ")
-        host = "host.docker.internal"
-        port = "11434"
-        url = f"http://{host}:{port}/api/chat"
-        model = self.MODEL
-        headers = {"Content-Type": "application/json"}
-        data = {
-            "model": model,  # 模型选择
-            "options": {
-                "temperature": 0.  # 为0表示不让模型自由发挥，输出结果相对较固定，>0的话，输出的结果会比较放飞自我
-            },
-            "stream": False,  # 流式输出
-            "messages": [{
-                "role": "system",
-                "content": user_input
-            }]  # 对话列表
-        }
-        response = requests.post(url, json=data, headers=headers, timeout=60)
-        res = response.json()
-        print(res)
--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from ChatApplication import *
 
 if __name__ == "__main__":
     DEBUG_MOD = 1
-    MODEL = "deepseek-r1:32b"
+    MODEL = "deepseek-r1:8b"
 
     app = ChatApplication(DEBUG_MOD, MODEL)
     app.run()
-    # app.call_ollama()
